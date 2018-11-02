3aad0433dc29488ae14c832179d60e0953009dbc
Siim Raudsepp <siimraudsepp@msn.com>
2018-11-02 22:05:52 +0200

    Bootstrapped initial Unity project

---
+++ Assets/Plugins/Editor/TexturePreProcessor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// Sets the correct fields for pixel art style sprites so it doesn't get filtered in any way
+public class TexturePreprocessor : AssetPostprocessor
+{
+
+    private static int PIXELSPERUNIT = 64;
+    
+    private void OnPreprocessTexture()
+    {    
+        var textureImporter = (TextureImporter) assetImporter;
+
+        var asset = AssetDatabase.LoadAssetAtPath(textureImporter.assetPath, typeof(Texture2D));
+        
+        // if not in asset database then it's a new asset and needs processing
+        if (!asset && textureImporter.textureType == TextureImporterType.Sprite)
+        {
+            textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+
+            textureImporter.filterMode = FilterMode.Point;
+
+            textureImporter.spritePixelsPerUnit = PIXELSPERUNIT;
+        }
+    }
+}
